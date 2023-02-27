705de4c39d1101d134dc596ac09fa963d70f6477
Brianne <bcwilhelmi@gmail.com>
2023-02-27 10:14:58 -0800

    changed fonts and color changing

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-function colorChangeRGB() {
+function colorChangeRG() {
     const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = 0;
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+function colorChangeGB() {
+    const r = 0;
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`
 }
+
+function colorChangeRB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = 0;
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+
 const h1 = document.querySelector("h1");
 const p = document.querySelector("p");
 const body = document.querySelector("body");
 
-const intervalH1 = setInterval(function () {
-    h1.style.color = colorChangeRGB();
+const h1NoBlue = setInterval(function () {
+    h1.style.color = colorChangeRG();
 }, 1000);
 
-const intervalP = setInterval(function () {
-    p.style.color = colorChangeRGB();
+const pNoRed = setInterval(function () {
+    p.style.color = colorChangeGB();
 }, 2000);
 
-const intervalBody = setInterval(function () {
-    body.style.backgroundColor = colorChangeRGB();
+const bodyNoGreen = setInterval(function () {
+    body.style.backgroundColor = colorChangeRB();
 }, 500);
